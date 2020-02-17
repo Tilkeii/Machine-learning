e4bd3b18f4817f9c8a079c21a41ab76f35a061f5
Tilkeii <florian@gouarin.info>
2020-02-17 18:55:01 +0100

    update

--- Lib/AlgorithmesMarchineLearning/RegleRosenblatt.cpp
+++ Lib/AlgorithmesMarchineLearning/RegleRosenblatt.cpp
@@ -1,20 +1,31 @@
 #include "pch.h"
 #include "RegleRosenblatt.h"
 
+
 extern "C" {
     __declspec(dllexport) int GiveMe42FromC() {
         return 42;
     }
 
-    __declspec(dllexport) int CreateModel() {
+    /*
+        count_feature : correspond par exemple à l'age, le poid, etc.
+    */
+    __declspec(dllexport) double* CreateLinearModel(int count_feature) {
+        double* weight = new double[count_feature + 1];
+        
+        for (size_t i = 0; i < count_feature + 1; i++)
+        {
+            weight[i] = (double) (rand() % 2 - 1);
+        }
+
+        return weight;
+    }
+
+    __declspec(dllexport) int TrainLinearModel() {
         return 0;
     }
 
-    __declspec(dllexport) int TrainModel() {
-        return 0;
-    }
-
-    __declspec(dllexport) int PredictModel() {
+    __declspec(dllexport) int PredictLinearModel() {
         return 0;
     }
 
--- Unity/Neuronal visualization/Assets/TestImportDll.cs
+++ Unity/Neuronal visualization/Assets/TestImportDll.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
@@ -7,10 +8,23 @@ public class TestImportDll : MonoBehaviour
 {
     [DllImport("AlgorithmesMarchineLearning")]
     private static extern int GiveMe42FromC();
-    
+
+    [DllImport("AlgorithmesMarchineLearning")]
+    private static extern IntPtr CreateLinearModel(int count_feature);
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log($"MyCDll : {GiveMe42FromC()}");
+
+        double[] model = new double[3];
+
+        Marshal.Copy(model, 0, CreateLinearModel(2), model.Length);
+
+        foreach (var item in model)
+        {
+            Debug.Log($"Test : {item}");
+        }
+
     }
 }
