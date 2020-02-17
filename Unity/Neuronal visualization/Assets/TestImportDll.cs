6e567ab896d8d63b5156d341d4dfae37e163457b
Tilkeii <florian@gouarin.info>
2020-02-17 15:59:48 +0100

    update

---
+++ Unity/Neuronal visualization/Assets/TestImportDll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestImportDll : MonoBehaviour
+{
+    [DllImport("AlgorithmesMarchineLearning")]
+    private static extern int GiveMe42FromC();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"MyCDll : {GiveMe42FromC()}");
+    }
+}
