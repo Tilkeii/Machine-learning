3a73820b61be68e4094d50b46ea580baa9eb2426
Tilkeii <florian@gouarin.info>
2020-02-18 12:08:57 +0100

    classification

--- Lib/AlgorithmesMarchineLearning/RegleRosenblatt.cpp
+++ Lib/AlgorithmesMarchineLearning/RegleRosenblatt.cpp
@@ -34,8 +34,38 @@ extern "C" {
         return 0;
     }
 
-    __declspec(dllexport) int PredictLinearModel(double *model, double input[], int count_feature) {
-        return 0;
+    /**
+        count_input : total input
+    */
+    __declspec(dllexport) double* PredictRegressionModel(double* model, double* input, int count_input, int count_feature, int biais) {
+
+        double *resultat = new double[count_input];
+        int cmpt = 0;
+
+        for (size_t i = 0; i < count_input - 1; i+=2)
+        {
+            resultat[cmpt] = biais + model[1] * input[i] + model[2] * input[i + 1];
+            cmpt++;
+        }
+        return resultat;
+    }
+
+    __declspec(dllexport) double* PredictLinearModel(
+        double *model, 
+        double *input,
+        int count_input,
+        int count_feature,
+        int biais
+    ) {
+        double* resultat = new double[count_input];
+        
+        double* regression = PredictRegressionModel(model, input, count_input, count_feature, biais);
+        for (size_t i = 0; i < count_input; i++)
+        {
+            resultat[i] = signbit(regression[i]);
+        }
+
+        return resultat;
     }
 
     __declspec(dllexport) int FreeLinearModel(double *model) {
