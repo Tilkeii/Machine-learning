26ecc0ae34f76cbf0c07bd8f48fa74d07a12a75a
Tilkeii <florian@gouarin.info>
2020-02-20 17:18:39 +0100

    update

--- Lib/AlgorithmesMarchineLearning/Multilayer.cpp
+++ Lib/AlgorithmesMarchineLearning/Multilayer.cpp
@@ -1,2 +1,20 @@
 #include "pch.h"
 #include "Multilayer.h"
+
+double*** CreateMultiLayerModel()
+{
+	double*** model;
+	return model;
+}
+
+void TrainMultiLayerRegressionModel()
+{
+}
+
+void TrainMultiLayerClassificationModel()
+{
+}
+
+void PredictMultiLayerClassificationModel()
+{
+}
--- Lib/AlgorithmesMarchineLearning/Multilayer.h
+++ Lib/AlgorithmesMarchineLearning/Multilayer.h
@@ -1,3 +1,11 @@
 #pragma once
+#include <stdlib.h>
+#include <math.h>
+#include <Eigen>
 
-
+extern "C" {
+    __declspec(dllexport) double*** CreateMultiLayerModel();
+    __declspec(dllexport) void TrainMultiLayerRegressionModel();
+    __declspec(dllexport) void TrainMultiLayerClassificationModel();
+    __declspec(dllexport) void PredictMultiLayerClassificationModel();
+}
