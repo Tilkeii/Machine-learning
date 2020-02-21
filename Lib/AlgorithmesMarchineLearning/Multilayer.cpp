b476e78a3b0c06cecd2649f27ef720c3461cfc18
Tilkeii <florian@gouarin.info>
2020-02-21 11:25:45 +0100

    update

--- Lib/AlgorithmesMarchineLearning/Multilayer.cpp
+++ Lib/AlgorithmesMarchineLearning/Multilayer.cpp
@@ -7,20 +7,20 @@ double*** CreateMultiLayerModel(
 	int count_feature)
 {
 	// Initialisation mémoire du network
-	double*** layers = new double** [count_layer];
+	double*** network = new double** [count_layer];
 
 	for (int layer = 0; layer < count_layer; layer++) // Layers
 	{
-		layers[layer] = new double* [count_neurones[layer]]; // Initialisation mémoire de la couche "i"
+		network[layer] = new double* [count_neurones[layer]]; // Initialisation mémoire de la couche "i"
 		for (int neurone = 0; neurone < count_neurones[layer]; neurone++) // Models = neuronnes
 		{
-			layers[layer][neurone] = new double[count_feature + 1]; // Initialisation mémoire du model "i, j" 
+			network[layer][neurone] = new double[count_feature + 1]; // Initialisation mémoire du model "i, j" 
 			double* weights = CreateLinearModel(count_feature); // Creation d'un model w1, w2, w3
-			layers[layer][neurone] = weights; // Attribution du model
+			network[layer][neurone] = weights; // Attribution du model
 		}
 	}
 
-	return layers;
+	return network;
 }
 
 void TrainMultiLayerRegressionModel()
